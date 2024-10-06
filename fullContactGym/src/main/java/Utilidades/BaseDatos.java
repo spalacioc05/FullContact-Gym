4b727210b831be2c30f12f3131555233e37735d3
JeronimoDuque <jeronimo.duque1@udea.edu.co>
2024-10-05 21:56:32 -0500

    e empezado la creacion de la clase para utilizar la base de datos

---
+++ fullContactGym/src/main/java/Utilidades/BaseDatos.java
@@ -0,0 +1,21 @@
+package Utilidades;
+
+import java.io.FileReader;
+import java.io.Reader;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+public class BaseDatos {
+    public static void pruebas() {
+        Reader in = new FileReader("fullContactGym\\base de datos.csv");
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+        for (CSVRecord record : records) {
+            String lastName = record.get("Last Name");
+            String firstName = record.get("First Name");
+        }
+    }
+
+    public static void main(String[] args) {
+        BaseDatos.pruebas();
+    } 
+}
