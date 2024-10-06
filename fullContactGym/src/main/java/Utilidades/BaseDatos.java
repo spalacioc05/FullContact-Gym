93430705afe312cc81fc0e0f78a0205347a9c915
JeronimoDuque <jeronimo.duque1@udea.edu.co>
2024-10-06 12:17:30 -0500

    cambie la ruta relativa de la base de datos

--- fullContactGym/src/main/java/Utilidades/BaseDatos.java
+++ fullContactGym/src/main/java/Utilidades/BaseDatos.java
@@ -7,7 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 
 public class BaseDatos {
     public static void pruebas() {
-        Reader in = new FileReader("fullContactGym\\base de datos.csv");
+        Reader in = new FileReader("base de datos.csv");
         Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
         for (CSVRecord record : records) {
             String lastName = record.get("Last Name");
